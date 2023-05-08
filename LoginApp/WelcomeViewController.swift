a0b2990782af5d8422cb9885c6074c7e051d5f90
Shalenikoff <KShalen@gmail.com>
2023-05-08 16:51:07 +0300

    Made customization for textFields

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -20,6 +20,7 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         emojiLabel.text = "\u{2665}"
         welcomeUserLabel.text = "Welcome, \(userName!)"
+        
     }
     
     //MARK: Actions
